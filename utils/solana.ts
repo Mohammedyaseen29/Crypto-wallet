e54a3c1d060310e937aa49fe7facead53f95e1d1
Yaseen <yaseen17861786@gmail.com>
2025-03-05 16:32:44 +0530

    added solana.ts

---
+++ utils/solana.ts
@@ -0,0 +1,10 @@
+import { Keypair } from "@solana/web3.js";
+import { mnemonicToSeed } from "bip39";
+import { derivePath } from "ed25519-hd-key";
+
+export async function getSolanaKeyPair(mnemonic:string,index = 0){
+    const seed = await mnemonicToSeed(mnemonic);
+    const path =  `m/44'/501'/${index}/0`;
+    const deriveKey = derivePath(path,seed.toString('hex')).key;
+    return Keypair.fromSeed(deriveKey);
+}
